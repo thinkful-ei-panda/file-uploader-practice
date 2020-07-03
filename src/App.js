f4cf1e9d627b96f43dc11d7a4d1fb606f080cec1
Matthew Rougely <matthewrougelysr@gmail.com>
2020-07-03 00:28:53 -0700

    File Uploader complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,63 @@
-import React from 'react';
+// import React, { Component } from 'react';
+// import './App.css';
 
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <h1>File Uploader</h1>  
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+import React, { Component } from 'react';
 import './App.css';
+import SearchBar from './SearchBar/SearchBar';
+import FilterableList from './FilterableList/FilterableList';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: '',
+      filterOption: 'All'
+    };
+  }
+
+  updateSearchTerm(term) {
+    this.setState({
+      searchTerm: term
+    })
+  }
+
+  updateFilterOption(option) {
+    this.setState({
+      filterOption: option
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <SearchBar
+        searchTerm={this.state.searchTerm}
+        filterOption={this.state.filterOption}
+        handleUpdate={term=>this.updateSearchTerm(term)}
+        handleFilterChange={option => this.updateFilterOption(option)}
+        />
+
+        <FilterableList 
+        files={this.props.files}
+        searchTerm={this.state.searchTerm}
+        filterOption={this.state.filterOption} 
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
